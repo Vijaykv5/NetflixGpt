de64754e367ba6faeeda4bcbb99c056519768952
vijay <vijaykv2228@gmail.com>
2023-09-02 23:46:47 +0530

    Browse Page Updated

---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { API_Options } from '../utils/constants';
+
+const VideoBackground = ({movieid}) => {
+    const [trailerid,setTrailerId]=useState(null);
+    const getMovieVideos= async()=>{
+        const data= await fetch("https://api.themoviedb.org/3/movie/"+movieid+"/videos?language=en-US",API_Options);
+        const json=await data.json();
+        //console.log(json.results[11]);
+
+        const filterData=json.results.filter(video=>video.type==="Trailer");
+        const trailer=filterData[0];
+        console.log("trailer -" ,trailer);
+        
+        setTrailerId(trailer?.key)
+    }
+    console.log("id :",trailerid);
+    useEffect(()=>{
+        getMovieVideos();
+    },[])
+  return (
+    <div className='w-full '>
+        <iframe className='w-screen aspect-video' src={"https://www.youtube.com/embed/"+trailerid+"?&autoplay=1&mute=1"} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+    </div>
+  )
+}
+
+export default VideoBackground
