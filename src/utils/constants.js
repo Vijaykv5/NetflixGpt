b62163e5174f302c0d093ffb5fc99af3a911b65d
vijay <vijaykv2228@gmail.com>
2023-09-09 00:50:08 +0530

    Update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,4 +15,4 @@ export const IMG_CDN = "https://image.tmdb.org/t/p/w500/"
 
 export const BG_IMG ="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
 
-export const OPENAI_API_KEY ="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";
+export const OPENAI_API_KEY ="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";
